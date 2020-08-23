076d376a755fb29401bff7c3edaf8e1faf72cf07
Victor Fernandes <victorfernandes91@hotmail.com>
2020-08-23 14:17:53 -0300

    fixing nav controller dependency

--- app/src/main/java/com/voidx/jsonplaceholder/feature/home/view/HomeFragment.kt
+++ app/src/main/java/com/voidx/jsonplaceholder/feature/home/view/HomeFragment.kt
@@ -5,14 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.voidx.jsonplaceholder.R
 import com.voidx.jsonplaceholder.databinding.FragmentHomeBinding
 import com.voidx.jsonplaceholder.feature.home.HomeCoordinator
 import org.koin.android.ext.android.inject
+import org.koin.core.parameter.parametersOf
 
 class HomeFragment : Fragment() {
 
-    private val coordinator: HomeCoordinator by inject()
+    private val coordinator: HomeCoordinator by inject {
+        parametersOf(findNavController())
+    }
 
     private lateinit var binding: FragmentHomeBinding
 
