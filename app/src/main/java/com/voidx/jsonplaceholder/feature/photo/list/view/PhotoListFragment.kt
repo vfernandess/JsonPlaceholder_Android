cb7e05893e25cf9cc7cfcdc19b19cfde25ad6055
Victor Fernandes <victorfernandes91@hotmail.com>
2020-08-23 14:18:36 -0300

    added photo pagination

--- app/src/main/java/com/voidx/jsonplaceholder/feature/photo/list/view/PhotoListFragment.kt
+++ app/src/main/java/com/voidx/jsonplaceholder/feature/photo/list/view/PhotoListFragment.kt
@@ -7,18 +7,21 @@ import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.voidx.jsonplaceholder.databinding.FragmentPhotoListBinding
 import com.voidx.jsonplaceholder.feature.photo.list.presentation.PhotoListViewModel
 import com.voidx.jsonplaceholder.presentation.State
+import com.voidx.jsonplaceholder.view.widget.recyclerview.EndlessRecyclerViewScrollListener
 import org.koin.androidx.scope.lifecycleScope
 import org.koin.androidx.viewmodel.scope.viewModel
 
-class PhotoListFragment: Fragment() {
+class PhotoListFragment : Fragment() {
 
     private val viewModel: PhotoListViewModel by lifecycleScope.viewModel(this)
     private val adapter: PhotoAdapter by lifecycleScope.inject()
 
     private lateinit var binding: FragmentPhotoListBinding
+    private lateinit var infiteScrollListener: EndlessRecyclerViewScrollListener
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,6 +34,18 @@ class PhotoListFragment: Fragment() {
         binding.lifecycleOwner = this
 
         binding.list.adapter = adapter
+
+        infiteScrollListener = EndlessRecyclerViewScrollListener(
+            5,
+            EndlessRecyclerViewScrollListener.DIRECTION_BOTTOM,
+            binding.list.layoutManager as LinearLayoutManager
+        )
+
+        infiteScrollListener.onLoadMore = {
+            viewModel.load()
+        }
+
+        binding.list.addOnScrollListener(infiteScrollListener)
 
         return binding.root
     }
@@ -51,21 +66,25 @@ class PhotoListFragment: Fragment() {
     }
 
     private fun transitionToErrorState() {
+        infiteScrollListener.loading = false
         binding.loading.visibility = GONE
         binding.list.visibility = GONE
     }
 
     private fun transitionToSuccessState() {
+        infiteScrollListener.loading = false
         binding.loading.visibility = GONE
         binding.list.visibility = VISIBLE
     }
 
     private fun transitionToEmptyState() {
+        infiteScrollListener.loading = false
         binding.loading.visibility = GONE
         binding.list.visibility = GONE
     }
 
     private fun transitionToLoadingState() {
+        infiteScrollListener.loading = true
         binding.loading.visibility = VISIBLE
         binding.list.visibility = GONE
     }
---
+++ app/src/main/java/com/voidx/jsonplaceholder/view/widget/recyclerview/EndlessRecyclerViewScrollListener.kt
@@ -0,0 +1,62 @@
+package com.voidx.jsonplaceholder.view.widget.recyclerview
+
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class EndlessRecyclerViewScrollListener(
+    private val visibleThreshold: Int,
+    private val direction: Int,
+    private val layoutManager: LinearLayoutManager
+) : RecyclerView.OnScrollListener() {
+
+    companion object {
+        const val DIRECTION_TOP = 0
+        const val DIRECTION_BOTTOM = 1
+    }
+
+    var onLoadMore: (() -> Unit)? = null
+    var loading = false
+
+    override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+        super.onScrolled(recyclerView, dx, dy)
+        val totalItemCount = layoutManager.itemCount
+        val visibleItem = getVisibleItem()
+        if (!loading
+            && totalItemCount > 0
+            && visibleItem > -1
+            && hasToLoadMore(visibleItem, totalItemCount)
+        ) {
+            onLoadMore?.invoke()
+            loading = true
+        }
+    }
+
+    private fun getVisibleItem(): Int {
+        return when (direction) {
+            DIRECTION_TOP -> {
+                layoutManager.findFirstVisibleItemPosition()
+            }
+            DIRECTION_BOTTOM -> {
+                layoutManager.findLastVisibleItemPosition()
+            }
+            else -> {
+                -1
+            }
+        }
+    }
+
+    private fun hasToLoadMore(visibleItem: Int, totalItemCount: Int): Boolean {
+        return when (direction) {
+            DIRECTION_TOP -> {
+                (visibleItem - visibleThreshold) <= 0
+            }
+            DIRECTION_BOTTOM -> {
+                totalItemCount <= (visibleItem + visibleThreshold)
+            }
+            else -> {
+                false
+            }
+        }
+    }
+
+}
